bcb42841a618ed6d6910b1ccca6d14e43b222ba0
DcNeeraj29 <dcns8818@gmail>
2026-01-14 15:56:39 +0530

    Day-1 & Day-2: audio capture and speech-to-text working

---
+++ src/record_audio.py
@@ -0,0 +1,23 @@
+import sounddevice as sd
+import numpy as np
+from scipy.io.wavfile import write
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent.parent
+
+audio_dir = project_root / "audio"
+audio_file = audio_dir/"test.wav"
+
+fs = 16000
+seconds = 5
+
+audio_dir.mkdir(parents=True, exist_ok=True)
+
+
+
+print("Recording...")
+audio = sd.rec(int(seconds * fs), samplerate = fs, channels = 1)
+sd.wait()
+
+write(audio_file,fs, audio)
+print(f"Recording complete. Audio saved to \n{audio_file}")
