66558407153ebe577e191d032233c758da397af4
DcNeeraj29 <dcns8818@gmail>
2026-03-25 22:28:35 +0530

    feat: integrate faster-whisper for faster ASR and improve system performance

--- src/asr/speech_to_text.py
+++ src/asr/speech_to_text.py
@@ -1,19 +1,23 @@
-import whisper
+from faster_whisper import WhisperModel
 import os
 
 print("Loading Whisper model...")
-model = whisper.load_model("base")
+model = WhisperModel("base", compute_type="int8")
 
 def transcribe_audio(audio_path, output_dir ="text"):
 
     print(f"Transcribing audio from {audio_path}...")
-    result = model.transcribe(audio_path)
-    
+    segments, _ = model.transcribe(audio_path)
+
+    text = ""
+    for segment in segments:
+        text += segment.text + " "
+    text = text.strip()    
     os.makedirs(output_dir, exist_ok = True)
     output_path = os.path.join(output_dir, "transcription.txt")
 
     with open(output_path, "w", encoding = "utf-8")as f:
-        f.write(result["text"])
+        f.write(text)
     
     print(f"Transcription saved to: {output_path}")
-    return result["text"]
+    return text
